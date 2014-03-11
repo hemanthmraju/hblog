b3ea9b703a3d034428226aef8e91666b04c95c4b
hemanthmraju <kumarhemanth.cs@gmail.com>
2014-03-11 17:26:54 +0530

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hblog::Application.config.secret_key_base = 'b70f98b83c9f1ed2eff5bf6816c8f3d4b3f2b1701bd17cf82e5b9e9df8b7217576f2e3b4783a613011d72eccd58eab51d935e3427486bf6fd52d6450b6525237'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hblog::Application.config.session_store :cookie_store, key: '_hblog_session'
